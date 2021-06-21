4030d89128cb6df06cbf4cd3516fb1153f7f055b
Jinoh <pjh92147154@gmail.com>
2021-06-21 15:58:20 +0900

    script edit

--- Prototype 1/Assets/Script/PlayerController.cs
+++ Prototype 1/Assets/Script/PlayerController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class PlayerController : MonoBehaviour
 {
@@ -11,6 +12,13 @@ public class PlayerController : MonoBehaviour
     // [SerializeField] private float speed = 20.0f;
     [SerializeField] private float horsePower = 20.0f;
     [SerializeField] private float turnSpeed = 45.0f;
+    [SerializeField] GameObject centerOfMass;
+    [SerializeField] TextMeshProUGUI speedometerText;
+    [SerializeField] TextMeshProUGUI rpmText;
+    [SerializeField] float rpm;
+    [SerializeField] float speed;
+    [SerializeField] List<WheelCollider> allWheels;
+    [SerializeField] int wheelsOnGround;
 
     public float horizontalInput;
     public float verticalInput;
@@ -20,6 +28,7 @@ public class PlayerController : MonoBehaviour
     void Awake()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerRb.centerOfMass = centerOfMass.transform.position;
         mainCam = Camera.main;
         mainCam.enabled = true;
         subCam.enabled = false;
@@ -28,54 +37,86 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (inputId == 1)
+        if (IsOnGround())
         {
-            horizontalInput = Input.GetAxis("Horizontal2");
-            verticalInput = Input.GetAxis("Vertical2");
-
-            if (Input.GetKeyDown(switchkey))
+            if (inputId == 1)
             {
-                /*
-                if (mainCam.enabled)
+                horizontalInput = Input.GetAxis("Horizontal2");
+                verticalInput = Input.GetAxis("Vertical2");
+
+                if (Input.GetKeyDown(switchkey))
                 {
-                    subCam.enabled = true;
-                    mainCam.enabled = false;
+                    /*
+                    if (mainCam.enabled)
+                    {
+                        subCam.enabled = true;
+                        mainCam.enabled = false;
+                    }
+                    else if (!mainCam.enabled)
+                    {
+                        subCam.enabled = false;
+                        mainCam.enabled = true;
+                    }
+                    */
+                    mainCam.enabled = !mainCam.enabled;
+                    subCam.enabled = !subCam.enabled;
                 }
-                else if (!mainCam.enabled)
-                {
-                    subCam.enabled = false;
-                    mainCam.enabled = true;
-                }
-                */
-                mainCam.enabled = !mainCam.enabled;
-                subCam.enabled = !subCam.enabled;
+
+                // Moves the vehicle foward based on vertical Input
+                // transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+                playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
+                // Roates the vehicle based on horizontal Input
+                transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+                speed = Mathf.Round(playerRb.velocity.magnitude * 2.237f);
+                speedometerText.SetText("Speed : " + speed + "mph");
+                rpm = Mathf.Round((speed % 30) * 40);
+                rpmText.SetText("RPM : " + rpm);
             }
 
-            // Moves the vehicle foward based on vertical Input
-            // transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-            playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
-            // Roates the vehicle based on horizontal Input
-            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
-        }
-
-        else if (inputId == 2)
-        {
-            // get player Input
-            horizontalInput = Input.GetAxis("Horizontal1");
-            verticalInput = Input.GetAxis("Vertical1");
-
-            if (Input.GetKeyDown(switchkey))
+            else if (inputId == 2)
             {
-                mainCam.enabled = !mainCam.enabled;
-                subCam.enabled = !subCam.enabled;
-            }
+                // get player Input
+                horizontalInput = Input.GetAxis("Horizontal1");
+                verticalInput = Input.GetAxis("Vertical1");
 
-            // Moves the vehicle foward based on vertical Input
-            // transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
-            playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
-            // Roates the vehicle based on horizontal Input
-            transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+                if (Input.GetKeyDown(switchkey))
+                {
+                    mainCam.enabled = !mainCam.enabled;
+                    subCam.enabled = !subCam.enabled;
+                }
+
+                // Moves the vehicle foward based on vertical Input
+                // transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+                playerRb.AddRelativeForce(Vector3.forward * verticalInput * horsePower);
+                // Roates the vehicle based on horizontal Input
+                transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+                speed = Mathf.Round(playerRb.velocity.magnitude * 2.237f);
+                speedometerText.SetText("Speed : " + speed + "mph");
+                rpm = Mathf.Round((speed % 30) * 40);
+                rpmText.SetText("RPM : " + rpm);
+            }
         }
-        
+    }
+
+    bool IsOnGround()
+    {
+        wheelsOnGround = 0;
+        foreach (WheelCollider wheel in allWheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnGround++;
+            }
+        }
+        if (wheelsOnGround == 4)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
