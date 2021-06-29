09ef37c71d808a5e8e0d5a01651788336fde89b9
Jinoh <pjh92147154@gmail.com>
2021-06-29 10:04:38 +0900

    20210629_1004

--- ModCube/Assets/ModTheCube/Cube.cs
+++ ModCube/Assets/ModTheCube/Cube.cs
@@ -5,19 +5,71 @@ using UnityEngine;
 public class Cube : MonoBehaviour
 {
     public MeshRenderer Renderer;
-    
+    private Vector3 newPosition;
+    private Vector3 newScale;
+    private Color newColor;
+    private float rotationSpeed;
+    private float speed = 1.0f;
+    private float startDelay = 1.0f;
+    private float chagneInterval = 2.0f;
+    private float locationRandom = 7.0f;
     void Start()
     {
         transform.position = new Vector3(3, 4, 1);
         transform.localScale = Vector3.one * 1.3f;
         
         Material material = Renderer.material;
-        
-        material.color = new Color(0.5f, 1.0f, 0.3f, 0.4f);
+
+        InvokeRepeating("TargetLotation", startDelay, chagneInterval);
+        InvokeRepeating("ChangeScale", startDelay, chagneInterval);
+        InvokeRepeating("ChangeRotationSpeed", startDelay, chagneInterval);
+        InvokeRepeating("ChangeColor", startDelay, chagneInterval);
     }
     
     void Update()
     {
-        transform.Rotate(10.0f * Time.deltaTime, 0.0f, 0.0f);
+        transform.Rotate(rotationSpeed * Time.deltaTime, 0.0f, 0.0f);
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * speed);
+        transform.localScale = Vector3.Lerp(transform.localScale, newScale, Time.deltaTime * speed);
+        Renderer.material.color = Color.Lerp(Renderer.material.color, newColor, Time.deltaTime * speed * 5);
+        
     }
+
+    // change location
+    void TargetLotation()
+    {
+        float x, y, z;
+        x = Random.Range(-locationRandom, locationRandom);
+        y = Random.Range(-locationRandom, locationRandom);
+        z = Random.Range(-locationRandom, locationRandom);
+        newPosition = new Vector3(x, y, z);
+    }
+
+    // change scale
+    void ChangeScale()
+    {
+        float x, y, z;
+        x = Random.Range(1.0f, 5.0f);
+        y = Random.Range(1.0f, 5.0f);
+        z = Random.Range(1.0f, 5.0f);
+        newScale = new Vector3(x, y, z);
+    }
+
+    // change rotation speed
+    void ChangeRotationSpeed()
+    {
+        rotationSpeed = Random.Range(1.0f, 200.0f);
+    }
+
+    // change material color
+    void ChangeColor()
+    {
+        newColor = new Color(RandomColor(), RandomColor(), RandomColor());
+    }
+    float RandomColor()
+    {
+        float randomColor = Random.Range(0.0f, 1.0f);
+        return randomColor;
+    }
+    // change material opacity
 }
